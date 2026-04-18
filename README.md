f3881acf1dae181a631403009ca37bf23a97861c
Shinobu420 <112190455+Shinobu420@users.noreply.github.com>
2026-04-18 16:35:09 +0200

    chore: desktop file, install script, readme

---
+++ README.md
@@ -0,0 +1,41 @@
+# CaelestiaBind – Caelestia Keybind Viewer
+
+## Screenshot
+<details>
+<summary>App screenshot</summary>
+
+![App screenshot](assets/CaelestiaBinds.png)
+
+</details>
+
+
+## Requirements
+
+- Linux + Hyprland running
+- `hyprctl` available in PATH
+- Rust toolchain: `cargo`
+- Caelestia shell: https://github.com/caelestia-dots/caelestia
+## Install / Build
+
+```bash
+# Clone
+git clone https://github.com/Shinobu420/CaelestiaBinds.git
+
+cd CaelestiaBinds
+
+# set execute permission
+chmod +x ./install.sh
+
+# run the installer
+./install.sh
+```
+
+## Usage
+- start the app via app-launcher, terminal (caelestia-binds), or keybind (Super+Enter)
+- Press `/` (can be changed) to focus the search bar.
+
+## License
+
+SourceCode: MIT
+
+Font: SIL OFL 1.1
---
+++ install.fish
@@ -0,0 +1,57 @@
+#!/usr/bin/env fish
+
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/applications
+mkdir -p ~/.local/share/icons
+
+echo "Building release..."
+cargo build --release
+
+if test $status -eq 0
+    echo "Installing files..."
+    
+    cp target/release/caelestia-binds ~/.local/bin/
+    
+    cp assets/Logo_B.png ~/.local/share/icons/Logo_B.png
+
+    cp caelestiabind.desktop ~/.local/share/applications/
+    
+    echo "Updating desktop database..."
+    update-desktop-database ~/.local/share/applications/
+
+    echo "Adding path to fish config..."
+    fish_add_path ~/.local/bin
+    echo "Adding path to bash config..."
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+    echo "Adding Keybinds..."
+    
+    # add window rules and keybinds to hyprland
+    set CONF_FILE "$HOME/.config/caelestia/hypr-user.conf"
+    set BIND_LINE "bind = Super, Return, exec, pkill -x caelestia-binds || ~/.local/bin/caelestia-binds"
+
+    mkdir -p (dirname $CONF_FILE)
+    touch $CONF_FILE
+
+    if not grep -qF "$BIND_LINE" $CONF_FILE
+        echo "Adding keybind to config..."
+        echo "$BIND_LINE" >> $CONF_FILE
+    else
+        echo "Keybind already exists in config. Skipping."
+    end
+    if not grep -q "caelestiabind-float-center" $CONF_FILE
+    echo "Adding windowrule to config..."
+    
+    echo "
+        windowrule {
+            name = caelestiabind-float-center
+            match:title = ^(CaelestiaBinds)\$
+            float = on
+            center = on
+        }" >> $CONF_FILE
+else
+    echo "Windowrule already exists. Skipping."
+end
+    echo "Installation complete!"
+else
+    echo "Cargo build failed."
+end
